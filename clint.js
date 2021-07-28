297dde04cfca06682fd0e9a01725dba3a8fa9ce8
Veasey <clintvee@gmail.com>
2021-07-28 22:40:20 +0100

    added silly quote

--- clint.js
+++ clint.js
@@ -1,31 +1,7 @@
 $( document ).ready(function() {
   displayEmail();
-
-  $('body').css('padding-top', $('.header').height());
-  $(document).on('scroll', function () {
-    styleHeader();
-  });
+  displayQuote();
 });
-
-function styleHeader() {
-  if ($(window).scrollTop() > 0) {
-    $('.header').css('font-size','1em');
-  } else {
-    $('.header').css('font-size','1.5em');
-  }
-  $('body').css('padding-top', $('.header').height());
-}
-
-$('.tablinks').click(function() {
-  var link = $(this).data('link');
-  $('.tablinks').removeClass('active');
-  $(this).addClass('active');
-  $('.tabcontent').hide();
-  $('#' + link).show();
-});
-
-$('.tablinks').first().addClass('active');
-$('.tabcontent').first().show();
 
 function displayEmail() {
   var first = 'clint';
@@ -34,4 +10,14 @@ function displayEmail() {
   var fullemail = first + '.' + second + '@' + email + '.com';
   var link = '<a mailto:"'+fullemail+'">'+fullemail+'</a>';
   $('.email').html('<p>' + link + '</p>');
+}
+
+function displayQuote() {
+  var quotes = [
+    'Taking it easy since 1990',
+    'Its not only feasible, it\'s Clint Veasible'
+  ];
+
+  var select = Math.floor(Math.random()*quotes.length);
+  $("#quote").text("\"" + quotes[select] + "\"");
 }
